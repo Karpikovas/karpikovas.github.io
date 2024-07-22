471a1362c50cb036cc7d291ac0414de641b52a3e
Sofi Karpikova <karpikova-sp@activebt.ru>
2024-07-22 14:32:47 +0300

    дз homework1 - typescript 1

--- src/homeworks/ts1/3_write.ts
+++ src/homeworks/ts1/3_write.ts
@@ -11,6 +11,15 @@
  * - id (строка)
  * - name (строка)
  * - photo (строка, необязательно)
+ * */
+
+type Category = {
+  id: string;
+  name: string;
+  photo?: string;
+};
+
+/*
  *
  * Продукт (Product) содержит
  * - id (строка)
@@ -22,17 +31,50 @@
  * - price (число)
  * - category (Категория)
  *
+ * */
+
+type Product = {
+  id: string;
+  name: string;
+  photo: string;
+  desc?: string;
+  createdAt: string;
+  oldPrice?: number;
+  price: number;
+  category: Category;
+};
+
+/*
  * Операция (Operation) может быть либо тратой (Cost), либо доходом (Profit)
  *
- * Трата (Cost) содержит
- * - id (строка)
- * - name (строка)
- * - desc (строка, необязательно)
- * - createdAt (строка)
- * - amount (число)
- * - category (Категория)
- * - type ('Cost')
- *
+
+ */
+
+type Operation = Cost | Profit;
+
+/*
+* Трата (Cost) содержит
+* - id (строка)
+* - name (строка)
+* - desc (строка, необязательно)
+* - createdAt (строка)
+* - amount (число)
+* - category (Категория)
+* - type ('Cost')
+*
+* */
+
+type Cost = {
+    id: string;
+    name: string;
+    desc?: string;
+    createdAt: string;
+    amount: number;
+    category: Category;
+    type: 'Cost'
+};
+
+/*
  * Доход (Profit) содержит
  * - id (строка)
  * - name (строка)
@@ -42,15 +84,50 @@
  * - category (Категория)
  * - type ('Profit')
  * */
+type Profit = {
+    id: string;
+    name: string;
+    desc?: string;
+    createdAt: string;
+    amount: number;
+    category: Category;
+    type: 'Profit'
+};
+
+const randomCategory: Category = {
+    id: 'string',
+        name: 'String',
+            photo: 'photo'
+};
 
 /**
  * Создает случайный продукт (Product).
  * Принимает дату создания (строка)
  * */
-// export const createRandomProduct = (createdAt: string) => {};
+export const createRandomProduct = (createdAt: string): Product => {
+    return {
+        id: 'string',
+        name: 'string',
+        photo: 'string',
+        desc: 'string',
+        createdAt: createdAt,
+        oldPrice: 123,
+        price: 123,
+        category: randomCategory
+    }
+};
 
 /**
  * Создает случайную операцию (Operation).
  * Принимает дату создания (строка)
  * */
-// export const createRandomOperation = (createdAt: string) => {};
+export const createRandomOperation = (createdAt: string): Operation => {
+    return {
+        id: 'string',
+        name: 'string',
+        createdAt: createdAt,
+        amount: 123,
+        category: randomCategory,
+        type: 'Profit'
+    }
+};
